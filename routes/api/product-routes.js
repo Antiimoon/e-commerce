616933256fe7e87c193ed18a5548861fb0af532b
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-02-20 21:33:44 -0800

    find all products

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,9 +4,21 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      include: [{ model: Category,
+        attributes: ['category_name'],
+      },
+    { model: Tag, through: ProductTag,
+    attributes: ['tag_name'], 
+    }],
+  });
+  res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
