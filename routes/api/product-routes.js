4600ba13125bfeda088fbd1686bfee9956764c82
Antoinette Munoz <93494243+Antiimoon@users.noreply.github.com>
2022-02-20 21:38:14 -0800

    get single product func

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -22,9 +22,21 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      attributes: ['id', 'product_name', 'price', 'stock'],
+      include: [{ model: Category,
+        attributes: ['category_name'],
+      },
+      { model: Tag, through: ProductTag,
+        attributes: ['tag_name'],
+      }],
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
